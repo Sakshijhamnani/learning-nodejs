d7852baee69c59117ae439941f381d0be6b43cde
Sakshijhamnani <sakshijhamnani@gmail.com>
2024-08-08 14:28:15 +0530

    Routing request added

--- app.js
+++ app.js
@@ -4,11 +4,37 @@ const hostname = '127.0.0.1';
 const port = 4001;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Sakshi');
+ 
+  res.setHeader('Content-Type', 'text/html');
+  const url=req.url;
+  if(url==='/'){
+    res.write('<html>')
+    res.write('<head><title>Node js</title></head>')
+    res.write('<body><h1>Welcome to my Node js Project</h1></body>')
+    res.write('</html>')
+    res.end();
+}
+else if(url==='/home'){
+    res.write('<html>')
+    res.write('<head><title>home</title></head>')
+    res.write('<body><h1>Welcome Home</h1></body>')
+    res.write('</html>')
+    res.end();
+}
+else if(url==='/about'){
+    res.write('<html>')
+    res.write('<head><title>about</title></head>')
+    res.write('<body><h1>Welcome to About Us Page</h1></body>')
+    res.write('</html>')
+    res.end();
+}
+else if(url==='/node'){
+    res.write('<html>')
+    res.write('<head><title>Node js</title></head>')
+    res.write('<body><h1>Welcome to my Node js Project</h1></body>')
+    res.write('</html>')
+    res.end();
+}
 });
 
-server.listen(port, hostname, () => {
-  console.log("sakshi")
-});
+server.listen(port);
